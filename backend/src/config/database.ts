9a2b9d80860facc0e9fc36d9da8a679e8dd594cc
José Bulies <jbulies1987@gmail.com>
2025-02-06 10:40:40 +0000

    Revert "No changes made to files"
    This reverts commit b304f32538e911128353fc7bf3891a9c35e0678a.

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const pool = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -31,3 +31,5 @@ const initializeDatabase = async () => {
 
 // Inicializar la conexión a la base de datos
 initializeDatabase();
+
+export default pool;
